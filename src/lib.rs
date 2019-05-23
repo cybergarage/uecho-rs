b1427695fc3b215095bd520ac2e04d24aecd369a
Satoshi Konno <skonno@yahoo-corp.jp>
2019-05-23 13:05:42 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+pub mod uecho;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/uecho/mod.rs
@@ -0,0 +1,10 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+pub mod device;
+pub mod esv;
+pub mod message;
+pub mod node;
+pub mod object;
+pub mod profile;
