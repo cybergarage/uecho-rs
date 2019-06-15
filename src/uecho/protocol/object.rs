2c6c13f4cf0c28bc501da517604653a867384c53
Satoshi Konno <skonno@cybergarage.org>
2019-06-15 23:17:00 +0900

    Add uecho::protocol and transport

--- src/uecho/protocol/object.rs
+++ src/uecho/protocol/object.rs
@@ -1,3 +1,11 @@
 // Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+pub struct Object {}
+
+impl Object {
+    pub fn new() -> Object {
+        Object {}
+    }
+}
