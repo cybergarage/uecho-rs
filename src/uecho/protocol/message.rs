a932c2b236b4089955b1ce177b7f58f51bd83c26
Satoshi Konno <skonno@cybergarage.org>
2019-06-18 20:22:27 +0900

    Update Message::parse()

--- src/uecho/protocol/message.rs
+++ src/uecho/protocol/message.rs
@@ -9,10 +9,27 @@ impl Message {
         Message {}
     }
 
-    pub fn parse(&self, mut buf: &[u8]) -> bool {
-        false
+    pub fn parse(&self, buf: &[u8]) -> bool {
+        if buf.len() <= 0 {
+            return false
+        }
+        true
     }
 }
 
 #[test]
-fn message_parse_test() {}
+fn message_parse_test() {
+    let test_msg_bytes = &[
+        //EHD1Echonet,
+		//EHD2Format1,
+        0xA0, 0xB0, 0xC0,
+		0xD0, 0xE0, 0xF0,
+		//ESVNotification,
+		//byte(opc),
+		//1, 1, 'a',
+		//2, 2, 'b', 'c',
+		//3, 3, 'c', 'd', 'e'
+        ];
+    let msg = Message::new();
+    assert!(msg.parse(test_msg_bytes));
+}
