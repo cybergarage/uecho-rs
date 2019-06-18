8b5e9f6a56dfb7e3e860ad7963e412741966b194
Satoshi Konno <skonno@cybergarage.org>
2019-06-18 20:37:50 +0900

    Add protocol.ESV constants

--- src/uecho/protocol/esv.rs
+++ src/uecho/protocol/esv.rs
@@ -1,3 +1,20 @@
 // Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+pub const ESV_WRITE_REQUEST: u8 = 0x60;
+pub const ESV_WRITE_REQUEST_RESPONSE_REQUIRED: u8 = 0x61;
+pub const ESV_READ_REQUEST: u8 = 0x62;
+pub const ESV_NOTIFICATION_REQUEST: u8 = 0x63;
+pub const ESV_WRITE_READ_REQUEST: u8 = 0x6E;
+pub const ESV_WRITE_RESPONSE: u8 = 0x71;
+pub const ESV_READ_RESPONSE: u8 = 0x72;
+pub const ESV_NOTIFICATION: u8 = 0x73;
+pub const ESV_NOTIFICATION_RESPONSE_REQUIRED: u8 = 0x74;
+pub const ESV_NOTIFICATION_RESPONSE: u8 = 0x7A;
+pub const ESV_WRITE_READ_RESPONSE: u8 = 0x7E;
+pub const ESV_WRITE_REQUEST_ERROR: u8 = 0x50;
+pub const ESV_WRITE_REQUEST_RESPONSE_REQUIRED_ERROR: u8 = 0x51;
+pub const ESV_READ_REQUEST_ERROR: u8 = 0x52;
+pub const ESV_NOTIFICATION_REQUEST_ERROR: u8 = 0x53;
+pub const ESV_WRITE_READ_REQUEST_ERROR: u8 = 0x5E;
