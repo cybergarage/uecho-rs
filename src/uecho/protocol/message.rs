846284af94fba0076051c6e58a5f8a2f123f110b
Satoshi Konno <skonno@cybergarage.org>
2019-06-19 21:55:51 +0900

    Update Message::parse()

--- src/uecho/protocol/message.rs
+++ src/uecho/protocol/message.rs
@@ -2,61 +2,39 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub struct Message {}
-
 pub const HEADER_EHD1_ECHONET: u8 = 0x10;
 pub const HEADER_EHD2_FORMAT1: u8 = 0x81;
+pub const FRAME_HEADER_SIZE: usize = (1 + 1 + 2);
+pub const FORMAT1_HEADER_SIZE: usize = (3 + 3 + 1 + 1);
+pub const FORMAT1_MIN_SIZE: usize = (FRAME_HEADER_SIZE + FORMAT1_HEADER_SIZE);
+pub const FORMAT1_PROPERTY_HEADER_SIZE: usize = 2;
+pub const TID_SIZE: usize = 2;
+pub const TID_MAX: usize = 65535;
+pub const EOJ_SIZE: usize = 3;
 
+pub struct Message {
+    tid: [u8; 2],
+    seoj: [u8; 3],
+    deoj: [u8; 3],
+}
 impl Message {
     pub fn new() -> Message {
-        Message {}
+        Message {
+            tid: [0, 0],
+            seoj: [0, 0, 0],
+            deoj: [0, 0, 0],
+        }
     }
 
-    pub fn parse(&self, buf: &[u8]) -> bool {
-        if buf.len() <= 0 {
+    pub fn parse(&self, msg: &[u8]) -> bool {
+        if msg.len() <= 0 {
             return false;
         }
+
+        if (msg[0] != HEADER_EHD1_ECHONET) || (msg[1] != HEADER_EHD2_FORMAT1) {
+            return false;
+        }
+
         true
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    // use super::*;
-    use crate::uecho::protocol::esv::*;
-    use crate::uecho::protocol::message::*;
-
-    #[test]
-    fn message_parse_test() {
-        let test_msg_bytes = &[
-            HEADER_EHD1_ECHONET,
-            HEADER_EHD2_FORMAT1,
-            0x00,
-            0x00,
-            0xA0,
-            0xB0,
-            0xC0,
-            0xD0,
-            0xE0,
-            0xF0,
-            ESV_NOTIFICATION,
-            3,
-            1,
-            1,
-            0x61,
-            2,
-            2,
-            0x62,
-            0x63,
-            3,
-            3,
-            0x64,
-            0x65,
-            0x66,
-        ];
-
-        let msg = Message::new();
-        assert!(msg.parse(test_msg_bytes));
     }
 }
