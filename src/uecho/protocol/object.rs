0f597a119914dc877ed492ce8ceb1c1639ae5a31
Satoshi Konno <skonno@cybergarage.org>
2019-07-14 23:30:39 +0900

    Update Message

--- src/uecho/protocol/object.rs
+++ src/uecho/protocol/object.rs
@@ -2,10 +2,29 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub struct Object {}
+use crate::uecho::protocol::message::*;
+//use crate::uecho::protocol::property::*;
+
+pub struct Object {
+    seoj: [u8; 3],
+    deoj: [u8; 3],
+    esv: u8,
+}
 
 impl Object {
     pub fn new() -> Object {
-        Object {}
+        Object {
+            seoj: [0, 0, 0],
+            deoj: [0, 0, 0],
+            esv: 0,
+        }
+    }
+
+    pub fn new_with(msg: &Message) -> Object {
+        Object {
+            seoj: [0, 0, 0],
+            deoj: [0, 0, 0],
+            esv: 0,
+        }
     }
 }
