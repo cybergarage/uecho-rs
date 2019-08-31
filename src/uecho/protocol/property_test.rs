daa57325e6bc3fcd29d71ef991a8ae4734092bcc
Satoshi Konno <skonno@cybergarage.org>
2019-09-01 03:02:40 +0900

    Update Property::parse()

--- src/uecho/protocol/property_test.rs
+++ src/uecho/protocol/property_test.rs
@@ -16,7 +16,8 @@ mod tests {
         let mut prop = Property::new();
 
         assert!(prop.parse(test_prop_bytes));
-        //assert_eq!(prop.code(), 10u8);
-        //assert_eq!(prop.size(), 1);
+        assert_eq!(prop.code(), 10u8);
+        assert_eq!(prop.size(), 1);
+        assert_eq!(prop.data()[0], 0x61u8)
     }
 }
