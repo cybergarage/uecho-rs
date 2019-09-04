d72094146fea70ad2af4189c08f5fb6c3a68df32
Satoshi Konno <skonno@cybergarage.org>
2019-09-05 00:26:47 +0900

    Add object.rs

---
+++ src/uecho/object_test.rs
@@ -0,0 +1,15 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#[test]
+
+#[cfg(test)]
+mod tests {
+
+    use crate::uecho::object::*;
+
+    #[test]
+    fn object_test() {
+    }
+}
