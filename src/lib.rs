60d881a701d3d68419a8339a6eb110734ee0c7ad
Satoshi Konno <skonno@cybergarage.org>
2019-09-05 00:40:37 +0900

    Update mod.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,4 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub mod uecho {
-    pub mod protocol;
-    pub mod transport;
-}
+pub mod uecho;
