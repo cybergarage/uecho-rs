436cd3e4d1fa9432e8ccd24bf3cd2fd91dab0164
Satoshi Konno <skonno@cybergarage.org>
2019-09-06 00:19:08 +0900

    Updat UnicastServer

--- src/uecho/transport/unicast_server.rs
+++ src/uecho/transport/unicast_server.rs
@@ -2,7 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub struct UnicastServer {}
+use std::net::UdpSocket;
+use std::ptr;
+
+pub struct UnicastServer {
+    //sock: &'a UdpSocket,
+}
 
 impl UnicastServer {
     pub fn new() -> UnicastServer {
