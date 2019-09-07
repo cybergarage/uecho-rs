16e00e2dcf0028cb7d874adead1b1545be53990e
Satoshi Konno <skonno@cybergarage.org>
2019-09-07 19:19:05 +0900

    Update Manager::start() and stop()

--- src/uecho/transport/manager.rs
+++ src/uecho/transport/manager.rs
@@ -2,18 +2,34 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub struct Manager {}
+use crate::uecho::transport::server::Server;
+
+pub struct Manager {
+    servers: Vec<Box<Server>>,
+}
 
 impl Manager {
     pub fn new() -> Manager {
-        Manager {}
+        Manager {
+            servers: Vec::new(),
+        }
     }
 
     pub fn start(&self) -> bool {
+        for n in 0..self.servers.len() {
+            if !self.servers[n].start() {
+                return false;
+            }
+        }
         true
     }
 
     pub fn stop(&self) -> bool {
+        for n in 0..self.servers.len() {
+            if !self.servers[n].stop() {
+                return false;
+            }
+        }
         true
     }
 }
