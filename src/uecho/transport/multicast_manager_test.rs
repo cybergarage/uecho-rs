af77fd0c17dbe9d383a84a36cfd8e28817c2c8dc
Satoshi Konno <skonno@cybergarage.org>
2020-06-06 17:50:26 +0900

    Add transport.MulticastManager

---
+++ src/uecho/transport/multicast_manager_test.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#[cfg(test)]
+mod tests {
+
+    use crate::uecho::transport::multicast_manager::*;
+
+    #[test]
+    fn multicast_manager_test() {
+        let mgr = MulticastManager::new();
+        assert!(mgr.start());
+        assert!(mgr.stop());
+    }
+}
