a3fd0baf245faa3bd8e8d8e05288bc6f8c4e76e3
Satoshi Konno <skonno@cybergarage.org>
2020-06-06 17:50:26 +0900

    Add transport.MulticastManager

--- src/uecho/transport/multicast_manager.rs
+++ src/uecho/transport/multicast_manager.rs
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub struct UnicastManager {
+pub struct MulticastManager {
 }
 
-impl UnicastManager {
-    pub fn new() -> UnicastManager {
-        UnicastManager {}
+impl MulticastManager {
+    pub fn new() -> MulticastManager {
+        MulticastManager {}
     }
 
     pub fn start(&self) -> bool {
