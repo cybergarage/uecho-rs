231c19ab65c368e1125231932f265c2b6bacd71e
Satoshi Konno <skonno@cybergarage.org>
2020-06-14 18:39:03 +0900

    Update UnicastManager

--- src/uecho/transport/unicast_manager.rs
+++ src/uecho/transport/unicast_manager.rs
@@ -2,18 +2,37 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub struct UnicastManager {}
+use crate::uecho::transport::unicast_tcp_server::UnicastTcpServer;
+use crate::uecho::transport::unicast_udp_server::UnicastUdpServer;
+
+pub struct UnicastManager {
+    udp_server: UnicastUdpServer,
+    tcp_server: UnicastTcpServer,
+}
 
 impl UnicastManager {
     pub fn new() -> UnicastManager {
-        UnicastManager {}
+        UnicastManager {
+            udp_server: UnicastUdpServer::new(),
+            tcp_server: UnicastTcpServer::new(),
+        }
     }
 
     pub fn start(&self) -> bool {
+        if !self.udp_server.start() {
+            self.stop();
+            return false;
+        }
+        if !self.tcp_server.start() {
+            self.stop();
+            return false;
+        }
         true
     }
 
     pub fn stop(&self) -> bool {
-        true
+        let mut ret = self.udp_server.stop();
+        ret |= self.tcp_server.stop();
+        ret
     }
 }
