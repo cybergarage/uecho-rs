5d7741c7e032ed25898c5db1f47cce3575592b75
Satoshi Konno <skonno@cybergarage.org>
2020-07-04 00:02:24 +0900

    Update UnicastUdpServer::send_message()

--- src/uecho/transport/unicast_udp_server_test.rs
+++ src/uecho/transport/unicast_udp_server_test.rs
@@ -5,12 +5,21 @@
 #[cfg(test)]
 mod tests {
 
+    use crate::uecho::protocol::esv::*;
+    use crate::uecho::protocol::message::*;
     use crate::uecho::transport::unicast_udp_server::*;
 
     #[test]
     fn unicast_udp_server_test() {
         let mut server = UnicastUdpServer::new();
         assert!(server.start());
+        let server_addr = server.local_addr();
+        assert!(server_addr.is_ok());
+
+        let mut msg = Message::new();
+        msg.set_esv(ESV_READ_REQUEST);
+        assert!(server.send_message(server_addr.unwrap(), &msg));
+
         assert!(server.stop());
     }
 }
