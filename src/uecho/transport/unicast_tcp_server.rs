07b6d471510badc3e6976bb7c5e8b8101478a3d5
Satoshi Konno <skonno@cybergarage.org>
2021-05-05 15:13:18 +0900

    Fix warnings

--- src/uecho/transport/unicast_tcp_server.rs
+++ src/uecho/transport/unicast_tcp_server.rs
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-use std::net::UdpSocket;
-use std::ptr;
-
 pub struct UnicastTcpServer {}
 
 impl UnicastTcpServer {
