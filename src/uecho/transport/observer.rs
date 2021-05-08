9629537733d4ef34021c52264d58abfb37d6efc7
Satoshi Konno <skonno@yahoo-corp.jp>
2021-05-08 14:14:25 +0900

    Add trasport::Observer

---
+++ src/uecho/transport/observer.rs
@@ -0,0 +1,9 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use crate::uecho::protocol::message::Message;
+
+pub trait Observer {
+    fn on_notify(&mut self, msg: &Message);
+}
