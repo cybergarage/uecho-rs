7571521681693ec242126430c289f377bd9cbd4b
Satoshi Konno <skonno@cybergarage.org>
2021-07-04 23:00:15 +0900

    Add uecho::log::logger

---
+++ src/uecho/log/logger_test.rs
@@ -0,0 +1,21 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#[cfg(test)]
+mod tests {
+
+    use log::*;
+
+    use crate::uecho::log::logger;
+
+    #[test]
+    fn logger_test() {
+        logger::init();
+        error!("");
+        warn!("");
+        info!("");
+        debug!("");
+        trace!("");
+    }
+}
