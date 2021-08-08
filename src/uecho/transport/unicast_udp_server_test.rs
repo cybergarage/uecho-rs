62b66b3204588be324eb7e5666e3638cfd857e84
Satoshi Konno <skonno@cybergarage.org>
2021-08-08 20:09:17 +0900

    Add UnicastUdpServer::bind()

--- src/uecho/transport/unicast_udp_server_test.rs
+++ src/uecho/transport/unicast_udp_server_test.rs
@@ -4,6 +4,7 @@
 
 #[cfg(test)]
 mod tests {
+    use std::net::IpAddr;
     use std::sync::Arc;
     use std::sync::Mutex;
     use std::thread;
@@ -11,6 +12,7 @@ mod tests {
 
     use crate::uecho::protocol::esv::*;
     use crate::uecho::protocol::message::*;
+    use crate::uecho::transport::interface::*;
     use crate::uecho::transport::unicast_udp_server::*;
 
     use crate::uecho::log::logger;
@@ -20,29 +22,38 @@ mod tests {
     fn unicast_udp_server_test() {
         logger::init();
 
-        const TEST_OBSERVER_COUNT: i32 = 10;
-        let counter = Arc::new(Mutex::new(0));
+        fn test_udp_server(ifaddr: IpAddr) {
+            logger::init();
 
-        let mut server = UnicastUdpServer::new();
+            const TEST_OBSERVER_COUNT: i32 = 10;
+            let counter = Arc::new(Mutex::new(0));
 
-        let observer = TestNotifyCounter::new(counter.clone());
-        assert!(server.add_observer(Box::new(observer)));
+            let mut server = UnicastUdpServer::new();
 
-        assert!(server.start());
-        thread::sleep(Duration::from_secs(1));
+            let observer = TestNotifyCounter::new(counter.clone());
+            assert!(server.add_observer(Box::new(observer)));
 
-        let mut msg = Message::new();
-        msg.set_esv(ESV_READ_REQUEST);
-        for _ in 0..TEST_OBSERVER_COUNT {
-            let server_addr = server.local_addr();
-            assert!(server_addr.is_ok());
-            assert!(server.send_message(server_addr.unwrap(), &msg));
+            assert!(server.bind(ifaddr));
+            assert!(server.start());
+            thread::sleep(Duration::from_secs(1));
+
+            let mut msg = Message::new();
+            msg.set_esv(ESV_READ_REQUEST);
+            for _ in 0..TEST_OBSERVER_COUNT {
+                let server_addr = server.local_addr();
+                assert!(server_addr.is_ok());
+                assert!(server.send_message(server_addr.unwrap(), &msg));
+            }
+
+            let wait_time = (TEST_OBSERVER_COUNT as u64) / 4;
+            thread::sleep(Duration::from_secs(wait_time));
+            assert_eq!(*counter.lock().unwrap(), TEST_OBSERVER_COUNT);
+
+            assert!(server.stop());
         }
 
-        let wait_time = (TEST_OBSERVER_COUNT as u64) / 4;
-        thread::sleep(Duration::from_secs(wait_time));
-        assert_eq!(*counter.lock().unwrap(), TEST_OBSERVER_COUNT);
-
-        assert!(server.stop());
+        for ifaddr in get_v4_interfaces() {
+            test_udp_server(ifaddr)
+        }
     }
 }
