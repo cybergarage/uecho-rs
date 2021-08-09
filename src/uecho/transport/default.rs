a3a5300be6a4ed96a7b8798cddb3aa5481211338
Satoshi Konno <skonno@cybergarage.org>
2021-08-09 16:33:39 +0900

    Add MULTICAST_V4_ADDRESS and MULTICAST_V6_ADDRESS

--- src/uecho/transport/default.rs
+++ src/uecho/transport/default.rs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-pub const MULTICAST_ADDRESS: &str = "224.0.23.0";
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+pub const MULTICAST_V4_ADDRESS: Ipv4Addr = Ipv4Addr::new(224, 0, 23, 0);
+pub const MULTICAST_V6_ADDRESS: Ipv6Addr = Ipv6Addr::new(0xff, 0x02, 0, 0, 0, 0, 0, 1);
 pub const PORT: u16 = 3610;
 pub const MAX_PACKET_SIZE: usize = 1024;
