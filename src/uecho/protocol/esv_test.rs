af3341714d2de4b3ad4da3d8e3a4a45747694772
Satoshi Konno <skonno@cybergarage.org>
2021-09-25 23:58:28 +0900

    Add esv_test()

---
+++ src/uecho/protocol/esv_test.rs
@@ -0,0 +1,17 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#[cfg(test)]
+mod tests {
+
+    use crate::uecho::transport::esv::*;
+
+    #[test]
+    fn esv_test() {
+        for esv in Esv::iter() {
+            let u8esv = Esv::to_u8(esv);
+            assert_eq!(esv, Esv::from_u8(u8esv));
+        }
+    }
+}
