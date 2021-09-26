c8922f63e014aa12ee0888abd2b5dba392587802
Satoshi Konno <skonno@cybergarage.org>
2021-09-26 18:20:23 +0900

    Add object_is_profile_object_code()

--- src/uecho/object.rs
+++ src/uecho/object.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+use crate::uecho::node_profile::*;
+
 pub struct Object {}
 
 pub type ObjectCode = u32;
@@ -10,4 +12,14 @@ impl Object {
     pub fn new() -> Object {
         Object {}
     }
+}
+
+pub fn object_is_profile_object_code(code: ObjectCode) -> bool {
+    if code == NodeProfileObject {
+        return true;
+    }
+    if code == NodeProfileObjectReadOnly {
+        return true;
+    }
+    return false;
 }
