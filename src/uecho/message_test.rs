783e733c0ee86b8252aa0e82cbb3de435f200fca
Satoshi Konno <skonno@cybergarage.org>
2021-09-26 15:05:57 +0900

    Add message_test()

---
+++ src/uecho/message_test.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#[test]
+
+#[cfg(test)]
+mod tests {
+
+    use crate::uecho::message::*;
+
+    #[test]
+    fn message_test() {
+        message_serarch_new();
+    }
+}
