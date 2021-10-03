dc242e92812a407e008ca7d3839440deca44de00
Satoshi Konno <skonno@cybergarage.org>
2021-10-03 19:03:29 +0900

    Add Obect::add_property()

--- src/uecho/object_test.rs
+++ src/uecho/object_test.rs
@@ -8,8 +8,15 @@
 mod tests {
 
     use crate::uecho::object::*;
+    use crate::uecho::property::*;
 
     #[test]
-    fn object_test() {
+    fn object_new_test() {
+        let obj = Object::new();
+        for n in 1..10 {
+            let prop = Property::new();
+            prop.set_code(n as PropertyCpde);
+            assert!(obj.add_property(prop));
+        }
     }
 }
---
+++ src/uecho/property_test.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2019 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#[test]
+
+#[cfg(test)]
+mod tests {
+
+    use crate::uecho::property::*;
+
+    #[test]
+    fn property_test() {
+        Property::new();
+    }
+}
