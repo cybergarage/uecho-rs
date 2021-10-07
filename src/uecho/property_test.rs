2bd3c2d7ba8747de85c4e996c3fd0c30d63d6ed1
Satoshi Konno <skonno@cybergarage.org>
2021-10-07 22:06:56 +0900

    Add Property::add_data()

--- src/uecho/property_test.rs
+++ src/uecho/property_test.rs
@@ -10,12 +10,24 @@ mod tests {
     use crate::uecho::property::*;
 
     #[test]
-    fn property_set_test() {
+    fn property_set_data_test() {
         let mut prop = Property::new();
         for n in 1..10 {
             let data = vec![0; n];
             prop.set_data(data);
             assert_eq!(prop.size(), n);
+        }
+    }
+
+    #[test]
+    fn property_add_data_test() {
+        let mut prop = Property::new();
+        let total_data_size = 0;
+        for n in 1..10 {
+            let data = vec![0; n];
+            prop.add_data(data);
+            total_data_size += n;
+            assert_eq!(prop.size(), total_data_size);
         }
     }
 }
