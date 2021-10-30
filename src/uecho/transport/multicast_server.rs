0348e741b535e30a1bd1042e668875abc55ee751
Satoshi Konno <skonno@cybergarage.org>
2021-10-31 02:00:52 +0900

    Update copyrights

--- src/uecho/transport/multicast_server.rs
+++ src/uecho/transport/multicast_server.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 The uecho-rs Authors. All rights reserved.
+// Copyright (C) 2021 The uecho-rs Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
