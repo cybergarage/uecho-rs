83792ca1742918c062a41d4743dea6ca5baac856
Satoshi Konno <skonno@cybergarage.org>
2021-10-31 02:00:52 +0900

    Update copyrights

--- src/uecho/class.rs
+++ src/uecho/class.rs
@@ -1,3 +1,3 @@
-// Copyright (C) 2019 Satoshi Konno. All rights reserved.
+// Copyright (C) 2021 Satoshi Konno. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
