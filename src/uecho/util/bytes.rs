77b1257b343b319ab7a9ef17b50c63300f080205
Satoshi Konno <skonno@cybergarage.org>
2021-10-31 02:08:28 +0900

    Update Bytes::from_i32() and to_u32()

---
+++ src/uecho/util/bytes.rs
@@ -0,0 +1,24 @@
+// Copyright (C) 2021 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+pub struct Bytes {}
+
+impl Bytes {
+    pub fn from_i32(val: u32, bytes: &mut [u8]) {
+        let bytes_size = bytes.len();
+        for n in 0..bytes_size {
+            let idx = (bytes_size - 1) - n;
+            bytes[idx] = ((val >> (n * 8)) & 0xFF) as u8;
+        }
+    }
+    pub fn to_u32(bytes: &[u8]) -> u32 {
+        let mut val = 0 as u32;
+        let bytes_size = bytes.len();
+        for n in 0..bytes_size {
+            let idx = (bytes_size - 1) - n;
+            val += (bytes[idx] as u32) << (n * 8);
+        }
+        val
+    }
+}
---
+++ src/uecho/util/mod.rs
@@ -0,0 +1,7 @@
+// Copyright (C) 2021 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+pub mod bytes;
+
+mod bytes_test;
