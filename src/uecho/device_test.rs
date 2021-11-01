c0502efe62e6a7d5d40c4bb32a94f05d757c7e6d
Satoshi Konno <skonno@cybergarage.org>
2021-11-01 23:55:14 +0900

    Add Object::set_fault_status() and fault_status()

--- src/uecho/device_test.rs
+++ src/uecho/device_test.rs
@@ -15,5 +15,6 @@ mod tests {
         assert_eq!(dev.operating_status().byte_data(), ObjectOperatingStatusOn);
         assert_eq!(dev.installation_location().byte_data(), DeviceInstallationLocationUnknown);
         assert_eq!(dev.standard_version().byte_data(), [0x01, 0x00, DeviceStandardVersion, 0x00]);
+        assert_eq!(dev.operating_status().byte_data(), DeviceNoFaultOccurred);
         }
 }
