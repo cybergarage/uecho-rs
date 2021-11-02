5a18e079b580d735541984e93cf8b60f6e791336
Satoshi Konno <skonno@cybergarage.org>
2021-11-02 13:41:06 +0900

    Update Object constants

--- src/uecho/device.rs
+++ src/uecho/device.rs
@@ -6,70 +6,70 @@ use crate::uecho::object::Object;
 use crate::uecho::property::*;
 use crate::uecho::super_object::*;
 
-pub const DeviceOperatingStatus: u8 = ObjectOperatingStatus;
-pub const DeviceInstallationLocation: u8 = 0x81;
-pub const DeviceStandardVersion: u8 = 0x82;
-pub const DeviceIdentificationNumber: u8 = 0x83;
-pub const DeviceMeasuredInstantaneousPowerConsumption: u8 = 0x84;
-pub const DeviceMeasuredCumulativePowerConsumption: u8 = 0x85;
-pub const DeviceManufacturerFaultCode: u8 = 0x86;
-pub const DeviceCurrentLimitSetting: u8 = 0x87;
-pub const DeviceFaultStatus: u8 = 0x88;
-pub const DeviceFaultDescription: u8 = 0x89;
-pub const DeviceManufacturerCode: u8 = ObjectManufacturerCode;
-pub const DeviceBusinessFacilityCode: u8 = 0x8B;
-pub const DeviceProductCode: u8 = 0x8C;
-pub const DeviceProductionNumber: u8 = 0x8D;
-pub const DeviceProductionDate: u8 = 0x8E;
-pub const DevicePowerSavingOperationSetting: u8 = 0x8F;
-pub const DeviceRemoteControlSetting: u8 = 0x93;
-pub const DeviceCurrentTimeSetting: u8 = 0x97;
-pub const DeviceCurrentDateSetting: u8 = 0x98;
-pub const DevicePowerLimitSetting: u8 = 0x99;
-pub const DeviceCumulativeOperatingTime: u8 = 0x9A;
-pub const DeviceAnnoPropertyMap: u8 = ObjectAnnoPropertyMap;
-pub const DeviceSetPropertyMap: u8 = ObjectSetPropertyMap;
-pub const DeviceGetPropertyMap: u8 = ObjectGetPropertyMap;
+pub const DEVICE_OPERATING_STATUS: u8 = OBJECT_OPERATING_STATUS;
+pub const DEVICE_INSTALLATION_LOCATION: u8 = 0x81;
+pub const DEVICE_STANDARD_VERSION: u8 = 0x82;
+pub const DEVICE_IDENTIFICATION_NUMBER: u8 = 0x83;
+pub const DEVICE_MEASURED_INSTANTANEOUS_POWER_CONSUMPTION: u8 = 0x84;
+pub const DEVICE_MEASURED_CUMULATIVE_POWER_CONSUMPTION: u8 = 0x85;
+pub const DEVICE_MANUFACTURER_FAULT_CODE: u8 = 0x86;
+pub const DEVICE_CURRENT_LIMIT_SETTING: u8 = 0x87;
+pub const DEVICE_FAULT_STATUS: u8 = 0x88;
+pub const DEVICE_FAULT_DESCRIPTION: u8 = 0x89;
+pub const DEVICE_MANUFACTURER_CODE: u8 = OBJECT_MANUFACTURER_CODE;
+pub const DEVICE_BUSINESS_FACILITY_CODE: u8 = 0x8B;
+pub const DEVICE_PRODUCT_CODE: u8 = 0x8C;
+pub const DEVICE_PRODUCTION_NUMBER: u8 = 0x8D;
+pub const DEVICE_PRODUCTION_DATE: u8 = 0x8E;
+pub const DEVICE_POWER_SAVING_OPERATION_SETTING: u8 = 0x8F;
+pub const DEVICE_REMOTE_CONTROL_SETTING: u8 = 0x93;
+pub const DEVICE_CURRENT_TIME_SETTING: u8 = 0x97;
+pub const DEVICE_CURRENT_DATE_SETTING: u8 = 0x98;
+pub const DEVICE_POWER_LIMIT_SETTING: u8 = 0x99;
+pub const DEVICE_CUMULATIVE_OPERATING_TIME: u8 = 0x9A;
+pub const DEVICE_ANNO_PROPERTY_MAP: u8 = OBJECT_ANNO_PROPERTY_MAP;
+pub const DEVICE_SET_PROPERTY_MAP: u8 = OBJECT_SET_PROPERTY_MAP;
+pub const DEVICE_GET_PROPERTY_MAP: u8 = OBJECT_GET_PROPERTY_MAP;
 
-pub const DeviceInstallationLocationSize: i32 = 1;
-pub const DeviceStandardVersionSize: i32 = 4;
-pub const DeviceIdentificationNumberSize: i32 = 9;
-pub const DeviceMeasuredInstantaneousPowerConsumptionSize: i32 = 2;
-pub const DeviceMeasuredCumulativePowerConsumptionSize: i32 = 4;
-pub const DeviceManufacturerFaultCodeSize: i32 = 255;
-pub const DeviceCurrentLimitSettingSize: i32 = 1;
-pub const DeviceFaultStatusSize: i32 = 1;
-pub const DeviceFaultDescriptionSize: i32 = 2;
-pub const DeviceManufacturerCodeSize: usize = ObjectManufacturerCodeSize;
-pub const DeviceBusinessFacilityCodeSize: i32 = 3;
-pub const DeviceProductCodeSize: i32 = 12;
-pub const DeviceProductionNumberSize: i32 = 12;
-pub const DeviceProductionDateSize: i32 = 4;
-pub const DevicePowerSavingOperationSettingSize: i32 = 1;
-pub const DeviceRemoteControlSettingSize: i32 = 1;
-pub const DeviceCurrentTimeSettingSize: i32 = 2;
-pub const DeviceCurrentDateSettingSize: i32 = 4;
-pub const DevicePowerLimitSettingSize: i32 = 2;
-pub const DeviceCumulativeOperatingTimeSize: i32 = 5;
-pub const DeviceAnnoPropertyMapSize: i32 = ObjectAnnoPropertyMapMaxSize;
-pub const DeviceSetPropertyMapSize: i32 = ObjectSetPropertyMapMaxSize;
-pub const DeviceGetPropertyMapSize: i32 = ObjectGetPropertyMapMaxSize;
+pub const DEVICE_INSTALLATION_LOCATION_SIZE: usize = 1;
+pub const DEVICE_STANDARD_VERSION_SIZE: usize = 4;
+pub const DEVICE_IDENTIFICATION_NUMBER_SIZE: usize = 9;
+pub const DEVICE_MEASURED_INSTANTANEOUS_POWER_CONSUMPTION_SIZE: usize = 2;
+pub const DEVICE_MEASURED_CUMULATIVE_POWER_CONSUMPTION_SIZE: usize = 4;
+pub const DEVICE_MANUFACTURER_FAULT_CODE_SIZE: usize = 255;
+pub const DEVICE_CURRENT_LIMIT_SETTING_SIZE: usize = 1;
+pub const DEVICE_FAULT_STATUS_SIZE: usize = 1;
+pub const DEVICE_FAULT_DESCRIPTION_SIZE: usize = 2;
+pub const DEVICE_MANUFACTURER_CODE_SIZE: usize = OBJECT_MANUFACTURER_CODE_SIZE;
+pub const DEVICE_BUSINESS_FACILITY_CODE_SIZE: usize = 3;
+pub const DEVICE_PRODUCT_CODE_SIZE: usize = 12;
+pub const DEVICE_PRODUCTION_NUMBER_SIZE: usize = 12;
+pub const DEVICE_PRODUCTION_DATE_SIZE: usize = 4;
+pub const DEVICE_POWER_SAVING_OPERATION_SETTING_SIZE: usize = 1;
+pub const DEVICE_REMOTE_CONTROL_SETTING_SIZE: usize = 1;
+pub const DEVICE_CURRENT_TIME_SETTING_SIZE: usize = 2;
+pub const DEVICE_CURRENT_DATE_SETTING_SIZE: usize = 4;
+pub const DEVICE_POWER_LIMIT_SETTING_SIZE: usize = 2;
+pub const DEVICE_CUMULATIVE_OPERATING_TIME_SIZE: usize = 5;
+pub const DEVICE_ANNO_PROPERTY_MAP_SIZE: usize = OBJECT_ANNO_PROPERTY_MAP_MAX_SIZE;
+pub const DEVICE_SET_PROPERTY_MAP_SIZE: usize = OBJECT_SET_PROPERTY_MAP_MAX_SIZE;
+pub const DEVICE_GET_PROPERTY_MAP_SIZE: usize = OBJECT_GET_PROPERTY_MAP_MAX_SIZE;
 
-pub const DeviceOperatingStatusOn: u8 = ObjectOperatingStatusOn;
-pub const DeviceOperatingStatusOff: u8 = ObjectOperatingStatusOff;
-pub const DeviceVersionAppendixA: u8 = 'A' as u8;
-pub const DeviceVersionAppendixB: u8 = 'B' as u8;
-pub const DeviceVersionAppendixC: u8 = 'C' as u8;
-pub const DeviceVersionAppendixD: u8 = 'D' as u8;
-pub const DeviceVersionAppendixE: u8 = 'E' as u8;
-pub const DeviceVersionAppendixF: u8 = 'F' as u8;
-pub const DeviceVersionAppendixG: u8 = 'G' as u8;
-pub const DeviceVersionUnknown: u8 = 0;
-pub const DeviceDefaultVersionAppendix: u8 = DeviceVersionAppendixG;
-pub const DeviceFaultOccurred: u8 = 0x41;
-pub const DeviceNoFaultOccurred: u8 = 0x42;
-pub const DeviceInstallationLocationUnknown: u8 = 0x00;
-pub const DeviceManufacturerUnknown: u32 = ObjectManufacturerUnknown;
+pub const DEVICE_OPERATING_STATUS_ON: u8 = OBJECT_OPERATING_STATUS_ON;
+pub const DEVICE_OPERATING_STATUS_OFF: u8 = OBJECT_OPERATING_STATUS_OFF;
+pub const DEVICE_VERSION_APPENDIX_A: u8 = 'A' as u8;
+pub const DEVICE_VERSION_APPENDIX_B: u8 = 'B' as u8;
+pub const DEVICE_VERSION_APPENDIX_C: u8 = 'C' as u8;
+pub const DEVICE_VERSION_APPENDIX_D: u8 = 'D' as u8;
+pub const DEVICE_VERSION_APPENDIX_E: u8 = 'E' as u8;
+pub const DEVICE_VERSION_APPENDIX_F: u8 = 'F' as u8;
+pub const DEVICE_VERSION_APPENDIX_G: u8 = 'G' as u8;
+pub const DEVICE_VERSION_UNKNOWN: u8 = 0;
+pub const DEVICE_DEFAULT_VERSION_APPENDIX: u8 = DEVICE_VERSION_APPENDIX_G;
+pub const DEVICE_FAULT_OCCURRED: u8 = 0x41;
+pub const DEVICE_NO_FAULT_OCCURRED: u8 = 0x42;
+pub const DEVICE_INSTALLATION_LOCATION_UNKNOWN: u8 = 0x00;
+pub const DEVICE_MANUFACTURER_UNKNOWN: u32 = OBJECT_MANUFACTURER_UNKNOWN;
 
 pub struct Device {}
 
@@ -81,64 +81,64 @@ impl Device {
     }
 
     fn add_mandatory_properties(obj: &mut Object) {
-        obj.set_property(ObjectOperatingStatus, PropertyAttributeReadAnno);
+        obj.set_property(OBJECT_OPERATING_STATUS, PROPERTY_ATTRIBUTE_READ_ANNO);
         obj.set_operating_status(true);
 
-        obj.set_property(DeviceInstallationLocation, PropertyAttributeReadAnno);
-        obj.set_installation_location(DeviceInstallationLocationUnknown);
+        obj.set_property(DEVICE_INSTALLATION_LOCATION, PROPERTY_ATTRIBUTE_READ_ANNO);
+        obj.set_installation_location(DEVICE_INSTALLATION_LOCATION_UNKNOWN);
 
-        obj.set_property(DeviceStandardVersion, PropertyAttributeRead);
-        obj.set_standard_version(DeviceStandardVersion);
+        obj.set_property(DEVICE_STANDARD_VERSION, PROPERTY_ATTRIBUTE_READ);
+        obj.set_standard_version(DEVICE_STANDARD_VERSION);
 
-        obj.set_property(DeviceFaultStatus, PropertyAttributeReadAnno);
+        obj.set_property(DEVICE_FAULT_STATUS, PROPERTY_ATTRIBUTE_READ_ANNO);
         obj.set_fault_status(false);
 
-        obj.set_property(DeviceManufacturerCode, PropertyAttributeRead);
+        obj.set_property(DEVICE_MANUFACTURER_CODE, PROPERTY_ATTRIBUTE_READ);
     }
 }
 
 impl Object {
     pub fn set_operating_status(&mut self, status: bool) -> bool {
         let status_byte: u8 = if status {
-            ObjectOperatingStatusOn
+            OBJECT_OPERATING_STATUS_ON
         } else {
-            ObjectOperatingStatusOff
+            OBJECT_OPERATING_STATUS_OFF
         };
-        self.set_property_byte(DeviceOperatingStatus, status_byte)
+        self.set_property_byte(DEVICE_OPERATING_STATUS, status_byte)
     }
 
     pub fn operating_status(&mut self) -> &mut Property {
-        self.property(DeviceOperatingStatus).unwrap()
+        self.property(DEVICE_OPERATING_STATUS).unwrap()
     }
 
     pub fn set_installation_location(&mut self, loc: u8) -> bool {
-        self.set_property_byte(DeviceInstallationLocation, loc)
+        self.set_property_byte(DEVICE_INSTALLATION_LOCATION, loc)
     }
 
     pub fn installation_location(&mut self) -> &mut Property {
-        self.property(DeviceInstallationLocation).unwrap()
+        self.property(DEVICE_INSTALLATION_LOCATION).unwrap()
     }
 
     pub fn set_standard_version(&mut self, ver: u8) -> bool {
         let mut vers: [u8; 4] = [0; 4];
         vers[2] = ver;
-        self.set_property_bytes(DeviceStandardVersion, &vers)
+        self.set_property_bytes(DEVICE_STANDARD_VERSION, &vers)
     }
 
     pub fn standard_version(&mut self) -> &mut Property {
-        self.property(DeviceStandardVersion).unwrap()
+        self.property(DEVICE_STANDARD_VERSION).unwrap()
     }
 
     pub fn set_fault_status(&mut self, status: bool) -> bool {
         let status_byte: u8 = if status {
-            DeviceFaultOccurred
+            DEVICE_FAULT_OCCURRED
         } else {
-            DeviceNoFaultOccurred
+            DEVICE_NO_FAULT_OCCURRED
         };
-        self.set_property_byte(DeviceFaultStatus, status_byte)
+        self.set_property_byte(DEVICE_FAULT_STATUS, status_byte)
     }
 
     pub fn fault_status(&mut self) -> &mut Property {
-        self.property(DeviceFaultStatus).unwrap()
+        self.property(DEVICE_FAULT_STATUS).unwrap()
     }
 }
--- src/uecho/profile.rs
+++ src/uecho/profile.rs
@@ -5,28 +5,28 @@
 use crate::uecho::object::*;
 use crate::uecho::super_object::*;
 
-pub const ProfileClassGroupCode: u8 = 0x0E;
+pub const PROFILE_CLASS_GROUP_CODE: u8 = 0x0E;
 
-pub const ProfileFaultStatus: u8 = 0x88;
-pub const ProfileManufacturerCode: u8 = ObjectManufacturerCode;
-pub const ProfilePlaceOfBusinessCode: u8 = 0x8B;
-pub const ProfileProductCode: u8 = 0x8C;
-pub const ProfileSerialNumber: u8 = 0x8D;
-pub const ProfileDateOfManufacture: u8 = 0x8E;
-pub const ProfileAnnoPropertyMap: u8 = ObjectAnnoPropertyMap;
-pub const ProfileSetPropertyMap: u8 = ObjectSetPropertyMap;
-pub const ProfileGetPropertyMap: u8 = ObjectGetPropertyMap;
+pub const PROFILE_FAULT_STATUS: u8 = 0x88;
+pub const PROFILE_MANUFACTURER_CODE: u8 = OBJECT_MANUFACTURER_CODE;
+pub const PROFILE_PLACE_OF_BUSINESS_CODE: u8 = 0x8B;
+pub const PROFILE_PRODUCT_CODE: u8 = 0x8C;
+pub const PROFILE_SERIAL_NUMBER: u8 = 0x8D;
+pub const PROFILE_DATE_OF_MANUFACTURE: u8 = 0x8E;
+pub const PROFILE_ANNO_PROPERTY_MAP: u8 = OBJECT_ANNO_PROPERTY_MAP;
+pub const PROFILE_SET_PROPERTY_MAP: u8 = OBJECT_SET_PROPERTY_MAP;
+pub const PROFILE_GET_PROPERTY_MAP: u8 = OBJECT_GET_PROPERTY_MAP;
 
-pub const ProfileFaultStatusLen: i32 = 1;
-pub const ProfileManufacturerCodeLen: usize = ObjectManufacturerCodeSize;
-pub const ProfilePlaceOfBusinessCodeLen: i32 = 3;
-pub const ProfileProductCodeLen: i32 = 12;
-pub const ProfileSerialNumberLen: i32 = 12;
-pub const ProfileDateOfManufactureLen: i32 = 4;
+pub const PROFILE_FAULT_STATUS_LEN: i32 = 1;
+pub const PROFILE_MANUFACTURER_CODE_LEN: usize = OBJECT_MANUFACTURER_CODE_SIZE;
+pub const PROFILE_PLACE_OF_BUSINESS_CODE_LEN: i32 = 3;
+pub const PROFILE_PRODUCT_CODE_LEN: i32 = 12;
+pub const PROFILE_SERIAL_NUMBER_LEN: i32 = 12;
+pub const PROFILE_DATE_OF_MANUFACTURE_LEN: i32 = 4;
 
-pub const ProfileFaultEncountered: u8 = 0x41;
-pub const ProfileNoFaultEncountered: u8 = 0x42;
-pub const ProfileManufacturerUnknown: u32 = ObjectManufacturerUnknown;
+pub const PROFILE_FAULT_ENCOUNTERED: u8 = 0x41;
+pub const PROFILE_NO_FAULT_ENCOUNTERED: u8 = 0x42;
+pub const PROFILE_MANUFACTURER_UNKNOWN: u32 = OBJECT_MANUFACTURER_UNKNOWN;
 
 pub struct Profile {}
 
