3904186af10cb55adeb88830d851aa727e5b6e14
Satoshi Konno <skonno@cybergarage.org>
2022-09-13 23:08:10 +0900

    Add TestDevice

---
+++ tests/device.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2021 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use uecho::device::Device;
+use uecho::object::Object;
+
+pub struct TestDevice {
+    obj: Object,
+}
+
+impl TestDevice {
+    pub fn new() -> TestDevice {
+        TestDevice { obj: Device::new() }
+    }
+}
