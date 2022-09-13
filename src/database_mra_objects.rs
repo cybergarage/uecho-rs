ddb44588e3a190a52e1973c07dfa2ae8ed3b817b
Satoshi Konno <skonno@cybergarage.org>
2022-09-14 08:26:27 +0900

    Add StandardDatabase::init_objects()

---
+++ src/database_mra_objects.rs
@@ -0,0 +1,10 @@
+// Copyright (C) 2021 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use crate::database::StandardDatabase;
+use crate::object::*;
+
+impl StandardDatabase {
+    pub fn init_objects(&mut self) {}
+}
