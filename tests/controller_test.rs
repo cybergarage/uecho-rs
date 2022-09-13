b0b2b07aebae78351544c85b87c9d2a051db8aa7
Satoshi Konno <skonno@cybergarage.org>
2022-09-13 23:17:05 +0900

    Add controller_test()

---
+++ tests/controller_test.rs
@@ -0,0 +1,17 @@
+// Copyright (C) 2021 The uecho-rs Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use uecho::controller::Controller;
+use uecho::log::logger;
+
+#[test]
+fn controller_test() {
+    logger::init();
+
+    let mut ctrl = Controller::new();
+    assert!(ctrl.start());
+    assert!(ctrl.start());
+    assert!(ctrl.search_all());
+    assert!(ctrl.stop());
+}
