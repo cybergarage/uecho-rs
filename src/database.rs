c6c302f84b2d87deff3781fd4d3898f60ff9a5a8
Satoshi Konno <skonno@cybergarage.org>
2022-09-14 08:27:28 +0900

    Update StandardDatabase::new()

--- src/database.rs
+++ src/database.rs
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-use crate::database_manufacturers::*;
-use crate::database_mra_objects::*;
 use crate::manufacture::*;
 use crate::object::*;
 
