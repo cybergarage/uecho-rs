dae719239d17998ea2c421fa1ec7e19f0f8920f3
Satoshi Konno <skonno@cybergarage.org>
2022-09-14 08:25:46 +0900

    Add StandardDatabase::init_manufactures()

---
+++ src/database_manufacturers.rs
@@ -0,0 +1,10 @@
+// Copyright (C) 2021 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use crate::database::StandardDatabase;
+use crate::object::*;
+
+impl StandardDatabase {
+    pub fn init_manufactures(&mut self) {}
+}
