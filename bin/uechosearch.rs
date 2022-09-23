d59d0b214d9d3b707279db9110400cce072794a1
Satoshi Konno <skonno@cybergarage.org>
2022-09-23 19:27:36 +0900

    Update uechosearch

--- bin/uechosearch.rs
+++ bin/uechosearch.rs
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-use uecho::controller::*;
+use uecho::controller::Controller;
 use uecho::log::*;
+use uecho::property::Property;
+use uecho::protocol::message::Message;
 
 fn main() {
     logger::init();
@@ -11,5 +13,18 @@ fn main() {
     let mut ctrl = Controller::new();
     ctrl.start();
     ctrl.search_all();
+
+    for node in ctrl.nodes() {
+        for obj in node.objects() {
+            for obj_prop in obj.properties() {
+                let mut msg = Message::new();
+                msg.set_destination_object_code(obj.code());
+                let mut prop = Property::new();
+                prop.set_code(obj_prop.code());
+                ctrl.send_message(node, &msg);
+            }
+        }
+    }
+
     ctrl.stop();
 }
