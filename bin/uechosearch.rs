af019268594cbbb6873359016dfd0bf41fcd7e66
Satoshi Konno <skonno@cybergarage.org>
2022-09-25 14:10:02 +0900

    Fix warning messages

--- bin/uechosearch.rs
+++ bin/uechosearch.rs
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-use std::sync::Arc;
-use std::sync::Mutex;
-
 use uecho::controller::Controller;
 use uecho::log::*;
 use uecho::property::Property;
