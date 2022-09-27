87db00869fa1ce72d9d0840f5749c46667385ee2
Satoshi Konno <skonno@cybergarage.org>
2022-09-27 13:53:54 +0900

    Add UdpSocket using nix::sys::socket

--- src/transport/udp_socket.rs
+++ src/transport/udp_socket.rs
@@ -3,8 +3,16 @@
 // license that can be found in the LICENSE file.
 
 use log::warn;
+use nix::sys::socket::sockopt::{IpAddMembership, ReuseAddr, ReusePort};
+use nix::sys::socket::{bind, recvfrom, sendto, setsockopt, shutdown, socket};
+use nix::sys::socket::{
+    AddressFamily, IpMembershipRequest, MsgFlags, Shutdown, SockFlag, SockType, SockaddrLike,
+};
+use nix::Result;
 use socket2::{Domain, Socket, Type};
 use std::io;
+use std::net::Ipv4Addr;
+use std::os::unix::io::RawFd;
 use std::thread;
 use std::time::Duration;
 
@@ -26,5 +34,54 @@ pub fn udp_socket_create() -> io::Result<Socket> {
 }
 
 pub fn udp_socket_closewait() {
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_secs(1));
+}
+
+pub struct UdpSocket {
+    sock: RawFd,
+}
+
+impl UdpSocket {
+    pub fn new() -> UdpSocket {
+        let sock = socket(
+            AddressFamily::Inet,
+            SockType::Datagram,
+            SockFlag::empty(),
+            None,
+        )
+        .unwrap();
+
+        if setsockopt(sock, ReuseAddr, &true).is_err() {
+            warn!("SO_REUSEADDR is not supported");
+        }
+        if setsockopt(sock, ReusePort, &true).is_err() {
+            warn!("SO_REUSEPORT is not supported");
+        }
+
+        UdpSocket { sock: sock }
+    }
+
+    pub fn close(&self) -> Result<()> {
+        let res = shutdown(self.sock, Shutdown::Both);
+        udp_socket_closewait();
+        res
+    }
+
+    pub fn bind(&self, addr: &dyn SockaddrLike) -> Result<()> {
+        bind(self.sock, addr)
+    }
+
+    pub fn send_to(&self, buf: &[u8], addr: &dyn SockaddrLike) -> Result<usize> {
+        let flags = MsgFlags::empty();
+        sendto(self.sock, buf, addr, flags)
+    }
+
+    pub fn recvfrom<T: SockaddrLike>(&self, buf: &mut [u8]) -> Result<(usize, Option<T>)> {
+        recvfrom(self.sock, buf)
+    }
+
+    pub fn join_multicast_v4(&self, multiaddr: Ipv4Addr, interface: Ipv4Addr) -> Result<()> {
+        let opt = IpMembershipRequest::new(multiaddr, Some(interface));
+        setsockopt(self.sock, IpAddMembership, &opt)
+    }
 }
