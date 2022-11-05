2d02986f5615181b0e00d4c790e9977554441cef
Satoshi Konno <skonno@cybergarage.org>
2022-11-05 16:56:31 +0900

    Update UdpSocket::close()

--- src/transport/udp_socket.rs
+++ src/transport/udp_socket.rs
@@ -10,7 +10,6 @@ use nix::sys::socket::{
     AddressFamily, IpMembershipRequest, MsgFlags, Shutdown, SockFlag, SockType, SockaddrIn,
 };
 use nix::Result;
-use socket2::{Domain, Socket, Type};
 use std::io;
 use std::net::Ipv4Addr;
 use std::net::{SocketAddr, SocketAddrV4};
@@ -18,27 +17,6 @@ use std::os::unix::io::RawFd;
 use std::str::FromStr;
 use std::thread;
 use std::time::Duration;
-
-pub fn udp_socket_create() -> io::Result<Socket> {
-    let socket = Socket::new(Domain::IPV4, Type::DGRAM, None);
-    match socket {
-        Ok(ref sock) => {
-            if sock.set_reuse_address(true).is_err() {
-                warn!("SO_REUSEADDR is not supported");
-            }
-            // NOTE: set_reuse_port() is not supported yet in socket2 v0.4.5.
-            // if sock.set_reuse_port(true).is_err() {
-            //     warn!("SO_REUSEPORT is not supported");
-            // }
-        }
-        Err(ref _err) => {}
-    }
-    socket
-}
-
-pub fn udp_socket_closewait() {
-    thread::sleep(Duration::from_secs(1));
-}
 
 pub struct UdpSocket {
     sock: RawFd,
@@ -87,7 +65,7 @@ impl UdpSocket {
 
     pub fn close(&self) -> Result<()> {
         let res = shutdown(self.sock, Shutdown::Both);
-        udp_socket_closewait();
+        thread::sleep(Duration::from_secs(1));
         res
     }
 
