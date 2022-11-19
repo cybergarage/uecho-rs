ccd0870c649a28950b6734433a4b4afd01ba93c6
Satoshi Konno <skonno@cybergarage.org>
2022-11-19 22:54:55 +0900

    Update UdpSocket using std::UdpSocket instead of nix::sys::socket

--- src/transport/multicast_server.rs
+++ src/transport/multicast_server.rs
@@ -6,7 +6,7 @@ use log::*;
 use std::io;
 use std::net::SocketAddr;
 use std::net::{IpAddr, Ipv4Addr};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 use crate::protocol::message::Message;
@@ -17,14 +17,14 @@ use crate::transport::observer::*;
 use crate::transport::udp_socket::UdpSocket;
 
 pub struct MulticastServer {
-    socket: Option<Arc<UdpSocket>>,
+    socket: Arc<Mutex<UdpSocket>>,
     notifier: Notifier,
 }
 
 impl MulticastServer {
     pub fn new() -> MulticastServer {
         MulticastServer {
-            socket: None,
+            socket: Arc::new(Mutex::new(UdpSocket::new())),
             notifier: notifier_new(),
         }
     }
@@ -39,61 +39,46 @@ impl MulticastServer {
         let msg_bytes = msg.bytes();
         let addr = to_addr.ip();
         let port = to_addr.port();
-        match &self.socket {
-            Some(socket) => {
-                info!(
-                    "MCST {} -> {}:{} ({})",
-                    socket.local_addr().unwrap(),
-                    addr,
-                    port,
-                    msg,
-                );
-                if socket.send_to(&msg_bytes, to_addr).is_err() {
-                    warn!("Couldn't notify message to {} {}", addr, port);
-                    return false;
-                }
-            }
-            None => {
-                let any = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0);
-                let mut socket = UdpSocket::new();
-                socket.bind(any).expect("failed to bind host socket");
-                info!(
-                    "MCST {} -> {}:{} ({})",
-                    socket.local_addr().unwrap(),
-                    addr,
-                    port,
-                    msg,
-                );
-                if socket.send_to(&msg_bytes, to_addr).is_err() {
-                    warn!("Couldn't notify message to {} {}", addr, port);
-                    return false;
-                }
-            }
+        info!(
+            "MCST {} -> {}:{} ({})",
+            self.socket.lock().unwrap().local_addr().unwrap(),
+            addr,
+            port,
+            msg,
+        );
+        if self
+            .socket
+            .lock()
+            .unwrap()
+            .send_to(&msg_bytes, to_addr)
+            .is_err()
+        {
+            warn!("Couldn't notify message to {} {}", addr, port);
+            return false;
         }
         true
     }
 
     pub fn local_addr(&self) -> io::Result<SocketAddr> {
-        match &self.socket {
-            Some(socket) => {
-                return socket.local_addr();
-            }
-            None => return Err(io::Error::new(io::ErrorKind::NotConnected, "")),
-        }
+        self.socket.lock().unwrap().local_addr()
     }
 
     pub fn bind(&mut self, ifaddr: IpAddr) -> bool {
-        let mut socket = UdpSocket::new();
         let addr = format!("{}:{}", ifaddr, PORT);
         let addr: SocketAddr = addr.parse().unwrap();
         debug!("BIND MCT {}", addr);
-        if socket.bind(addr).is_err() {
-            self.socket = None;
+        if self.socket.lock().unwrap().bind(addr).is_err() {
             return false;
         }
         match ifaddr {
             IpAddr::V4(ip4) => {
-                if socket.join_multicast_v4(MULTICAST_V4_ADDRESS, ip4).is_err() {
+                if self
+                    .socket
+                    .lock()
+                    .unwrap()
+                    .join_multicast_v4(&MULTICAST_V4_ADDRESS, &ip4)
+                    .is_err()
+                {
                     self.close();
                     return false;
                 }
@@ -103,7 +88,13 @@ impl MulticastServer {
                 );
             }
             IpAddr::V6(ip6) => {
-                if socket.join_multicast_v6(MULTICAST_V6_ADDRESS, ip6).is_err() {
+                if self
+                    .socket
+                    .lock()
+                    .unwrap()
+                    .join_multicast_v6(&MULTICAST_V6_ADDRESS, &ip6)
+                    .is_err()
+                {
                     self.close();
                     return false;
                 }
@@ -113,42 +104,29 @@ impl MulticastServer {
                 );
             }
         }
-        self.socket = Some(Arc::new(UdpSocket::from(socket)));
         true
     }
 
     pub fn close(&mut self) -> bool {
-        if self.socket.is_some() {
-            let socket = self.socket.clone();
-            socket.unwrap().close();
-            self.socket = None;
-        }
+        self.socket.lock().unwrap().close();
         true
     }
 
     pub fn start(&mut self) -> bool {
-        if self.socket.is_none() {
-            return false;
-        }
-        let socket = self.socket.clone().unwrap();
+        let socket = self.socket.clone();
         let notifier = self.notifier.clone();
         thread::spawn(move || {
             let mut buf = [0 as u8; MAX_PACKET_SIZE];
             loop {
-                let recv_res = socket.recv_from(&mut buf);
+                let recv_res = socket.lock().unwrap().recv_from(&mut buf);
                 match &recv_res {
                     Ok((n_bytes, remote_addr)) => {
-                        let mut msg_addr =
-                            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), PORT);
-                        if remote_addr.is_some() {
-                            msg_addr = remote_addr.unwrap();
-                        }
                         let recv_msg = &buf[0..*n_bytes];
                         let mut msg = Message::new();
                         if !msg.parse(recv_msg) {
                             warn!(
                                 "Couldn't parse message {} [{}] {}",
-                                msg_addr,
+                                remote_addr,
                                 n_bytes,
                                 hex::encode_upper(recv_msg)
                             );
@@ -156,15 +134,19 @@ impl MulticastServer {
                         }
                         info!(
                             "RECV {} -> {} ({})",
-                            msg_addr,
-                            socket.local_addr().ok().unwrap(),
+                            remote_addr,
+                            socket.lock().unwrap().local_addr().ok().unwrap(),
                             msg
                         );
-                        msg.set_addr(msg_addr.ip());
+                        msg.set_addr(remote_addr.ip());
                         notifier.lock().unwrap().notify(&msg);
                     }
                     Err(e) => {
-                        error!("RECV {} -> {}", socket.local_addr().ok().unwrap(), e);
+                        error!(
+                            "RECV {} -> {}",
+                            socket.lock().unwrap().local_addr().ok().unwrap(),
+                            e
+                        );
                         break;
                     }
                 }
