4b1684d7eb2877cc7162decd9f9789ad10f134ef
Satoshi Konno <skonno@cybergarage.org>
2022-11-20 21:35:44 +0900

    Update UdpSocket::join_multicast_v?() to set the join interface

--- tests/controller_test.rs
+++ tests/controller_test.rs
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+use std::{thread, time};
 use uecho::controller::Controller;
 use uecho::log::logger;
 
@@ -10,8 +11,8 @@ fn controller_test() {
     logger::init();
 
     let mut ctrl = Controller::new();
-    // ctrl.add_observer(Arc::new(Mutex::new(ctrl)));
     assert!(ctrl.start());
     assert!(ctrl.search_all());
+    thread::sleep(time::Duration::from_secs(2));
     assert!(ctrl.stop());
 }
