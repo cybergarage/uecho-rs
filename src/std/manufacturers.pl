dc1f67900f4f1d8194c435ce1aabca66440fc0e7
Satoshi Konno <skonno@cybergarage.org>
2022-11-20 23:46:31 +0900

    Update LICENSE

--- src/std/manufacturers.pl
+++ src/std/manufacturers.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/perl
-# Copyright (C) 2018 The uecho-rs Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
+# Copyright (C) 2022 The uecho-rs Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 if (@ARGV < 1){
   exit 1;
@@ -9,9 +19,19 @@ if (@ARGV < 1){
 my $manlist_filename = $ARGV[0];
 
 print<<HEADER;
-// Copyright (C) 2021 The uecho-rs Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright (C) 2022 The uecho-rs Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use crate::database::StandardDatabase;
 use crate::manufacture::*;
