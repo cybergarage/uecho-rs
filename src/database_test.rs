b2dea009ad226b9bf78ceebcf9481b3a2c725502
Satoshi Konno <skonno@cybergarage.org>
2022-11-21 08:29:01 +0900

    Update object::find_property()

--- src/database_test.rs
+++ src/database_test.rs
@@ -26,19 +26,19 @@ mod tests {
         // Super Object
         let obj = db.find_object(SUPER_OBJECT_CODE);
         assert!(obj.is_some());
-        // let prop = obj.property(OBJECT_OPERATING_STATUS);
-        // assert(prop.is_some())ß
+        // let prop = obj.find_property(OBJECT_OPERATING_STATUS);
+        // assert(prop.is_some());
 
         // Node Profile
         // let obj = db.find_object(NODE_PROFILE_OBJECT_CODE);
         // assert!(obj.is_some());
-        // let prop = obj.property(NODE_PROFILE_CLASS_NUMBER_OF_SELF_NODE_INSTANCES);
+        // let prop = obj.find_property(NODE_PROFILE_CLASS_NUMBER_OF_SELF_NODE_INSTANCES);
         // assert(prop.is_some());
 
         // Mono functional lighting (0x0291)
         let obj = db.find_object(0x029100);
         assert!(obj.is_some());
-        // let prop = obj.property(0xB0); // Light level Setting
+        // let prop = obj.find_property(0xB0); // Light level Setting
         // assert(prop.is_some());
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -114,12 +114,12 @@ impl Object {
         self.properties.values()
     }
 
-    pub fn property(&mut self, code: PropertyCode) -> Option<&mut Property> {
+    pub fn find_property(&mut self, code: PropertyCode) -> Option<&mut Property> {
         self.properties.get_mut(&code)
     }
 
     pub fn set_property_data(&mut self, code: PropertyCode, data: &[u8]) -> bool {
-        match self.property(code).as_mut() {
+        match self.find_property(code).as_mut() {
             Some(prop) => {
                 prop.set_data(data);
                 true
@@ -144,7 +144,7 @@ impl Object {
     }
 
     pub fn property_data(&mut self, code: PropertyCode) -> Option<&PropertyData> {
-        match self.property(code) {
+        match self.find_property(code) {
             Some(prop) => return Some(prop.data()),
             None => return None,
         }
@@ -166,7 +166,7 @@ impl Object {
     }
 
     pub fn equals_property_data(&mut self, code: PropertyCode, data: &[u8]) -> bool {
-        match self.property(code) {
+        match self.find_property(code) {
             Some(prop) => prop.equals_data(data),
             None => false,
         }
