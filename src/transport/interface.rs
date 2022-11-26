941efdc5296f8491ebf0d6d0e9445a693bb83d07
Satoshi Konno <skonno@cybergarage.org>
2022-11-26 22:57:14 +0900

    Add #![allow(dead_code)]

--- src/transport/interface.rs
+++ src/transport/interface.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(dead_code)]
+
 use pnet::datalink;
 use pnet::ipnetwork;
 use std::net::IpAddr;
