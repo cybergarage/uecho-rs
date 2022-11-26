20eb2f4dc0383f44eca6b39858a5c54157d6780e
Satoshi Konno <skonno@cybergarage.org>
2022-11-27 03:14:35 +0900

    Update NodeProfile::update()

--- src/node_profile.rs
+++ src/node_profile.rs
@@ -57,7 +57,7 @@ pub const NODE_PROFILE_CLASS_NOT_BOOTING: u8 = 0x31;
 pub const LOWER_COMMUNICATION_LAYER_PROTOCOL_TYPE: u8 = 0xFE;
 
 pub struct NodeProfile<'a> {
-    obj: &'a Object,
+    obj: &'a mut Object,
 }
 
 impl NodeProfile<'_> {
@@ -68,9 +68,75 @@ impl NodeProfile<'_> {
         obj
     }
 
-    pub fn from(obj: &Object) -> NodeProfile {
+    pub fn from(obj: &mut Object) -> NodeProfile {
         NodeProfile { obj: obj }
     }
 
-    pub fn update(&mut self, objs: &Vec<ObjectCode>) {}
+    pub fn update(&mut self, obj_codes: &Vec<ObjectCode>) {
+        let num_class_codes = obj_codes.len() as u8;
+        let num_obj_codes = (obj_codes.len() - 1) as u8;
+
+        let mut class_list_bytes = Vec::new();
+        class_list_bytes.push(num_class_codes);
+        for obj_code in obj_codes {
+            class_list_bytes.push(((obj_code & 0xFF0000) >> 16) as u8);
+            class_list_bytes.push(((obj_code & 0x00FF00) >> 8) as u8);
+        }
+
+        let mut instance_list_bytes = Vec::new();
+        instance_list_bytes.push(num_obj_codes);
+        for obj_code in obj_codes {
+            if *obj_code == NODE_PROFILE_OBJECT_CODE {
+                continue;
+            }
+            instance_list_bytes.push(((obj_code & 0xFF0000) >> 16) as u8);
+            instance_list_bytes.push(((obj_code & 0x00FF00) >> 8) as u8);
+            instance_list_bytes.push((obj_code & 0x0000FF) as u8);
+        }
+
+        // Number of self-node instances (0xD3)
+        let num_instances_prop = self
+            .obj
+            .find_property_mut(NODE_PROFILE_CLASS_NUMBER_OF_SELF_NODE_INSTANCES);
+        if num_instances_prop.is_some() {
+            let num_instances_prop = num_instances_prop.unwrap();
+            num_instances_prop.set_integer_data(num_obj_codes.into(), 3);
+        }
+
+        // Number of self-node classes (0xD4)
+        let num_classes_prop = self
+            .obj
+            .find_property_mut(NODE_PROFILE_CLASS_NUMBER_OF_SELF_NODE_CLASSES);
+        if num_classes_prop.is_some() {
+            let num_classes_prop = num_classes_prop.unwrap();
+            num_classes_prop.set_integer_data(num_class_codes.into(), 2);
+        }
+
+        // Instance list notification (0xD5)
+        let instances_list_prop = self
+            .obj
+            .find_property_mut(NODE_PROFILE_CLASS_SELF_NODE_INSTANCE_LIST_S);
+        if instances_list_prop.is_some() {
+            let instances_list_prop = instances_list_prop.unwrap();
+            instances_list_prop.set_bytes_data(&instance_list_bytes);
+        }
+
+        // Self-node instance list S (D6)
+        let notification_instances_list_prop = self
+            .obj
+            .find_property_mut(NODE_PROFILE_CLASS_INSTANCE_LIST_NOTIFICATION);
+        if notification_instances_list_prop.is_some() {
+            let notification_instances_list_prop = notification_instances_list_prop.unwrap();
+            notification_instances_list_prop.set_bytes_data(&instance_list_bytes);
+        }
+
+        // Self-node class list S (0xD7)
+        let class_list_prop = self
+            .obj
+            .find_property_mut(NODE_PROFILE_CLASS_SELF_NODE_CLASS_LIST_S);
+        if class_list_prop.is_some() {
+            let class_list_prop = class_list_prop.unwrap();
+            class_list_prop.set_bytes_data(&class_list_bytes);
+        }
+    }
 }
