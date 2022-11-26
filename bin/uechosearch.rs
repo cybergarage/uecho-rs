a1281cffbda3a09f1586e23cd3de45d0d73527e3
Satoshi Konno <skonno@cybergarage.org>
2022-11-26 20:08:33 +0900

    Update uechosearch

--- bin/uechosearch.rs
+++ bin/uechosearch.rs
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::string::String;
 use std::time::Duration;
 use std::{thread, time};
 
 use echonet::log::*;
-use echonet::protocol::Esv;
-use echonet::protocol::Message;
-use echonet::protocol::Property;
-use echonet::Controller;
+use echonet::protocol::{Esv, Message, Property};
+use echonet::util::Bytes;
+use echonet::{Controller, ManufactureCode, StandardDatabase};
 
 fn main() {
     // logger::init();
@@ -31,7 +31,36 @@ fn main() {
     thread::sleep(time::Duration::from_secs(2));
 
     for (i, node) in ctrl.nodes().iter().enumerate() {
-        println!("[{}] {}", i, node.addr());
+        // Makes a manufacture code read (Esv::ReadRequest) message.
+        let mut msg = Message::new();
+        msg.set_esv(Esv::ReadRequest);
+        msg.set_deoj(0x0EF001);
+        let mut prop = Property::new();
+        prop.set_code(0x8A);
+        msg.add_property(prop);
+
+        let mut manufacture_name = String::from("");
+        let rx = ctrl.post_message(&node, &mut msg);
+        match rx.recv_timeout(Duration::from_secs(1)) {
+            Ok(msg) => {
+                let props = msg.properties();
+                if 0 < props.len() {
+                    let manufacture_code = Bytes::to_u32(props[0].data()) as ManufactureCode;
+                    let std_db = StandardDatabase::shared();
+                    let found_manufacture = std_db.find_manufacture(manufacture_code);
+                    if found_manufacture.is_some() {
+                        manufacture_name = found_manufacture.unwrap().name().to_string();
+                    }
+                }
+            }
+            Err(_e) => {
+            }
+        };
+
+        println!("[{}] {} ({})", i, node.addr(), manufacture_name);
+
+        // Prints all mandatory properties in the object.
+
         for (j, obj) in node.objects().iter().enumerate() {
             println!("    [{}] {:06X} ({})", j, obj.code(), obj.class_name());
             for obj_prop in obj.properties() {
@@ -39,6 +68,7 @@ fn main() {
                     continue;
                 }
 
+                // Makes a property value read (Esv::ReadRequest) message.
                 let mut msg = Message::new();
                 msg.set_esv(Esv::ReadRequest);
                 msg.set_deoj(obj.code());
