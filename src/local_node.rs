b2c5966534f5edbf989a587182a08f9eca0b51f2
Satoshi Konno <skonno@cybergarage.org>
2022-11-27 01:30:24 +0900

    Update LocalNode::find_object()

--- src/local_node.rs
+++ src/local_node.rs
@@ -65,7 +65,7 @@ impl LocalNode {
         return &self.objects;
     }
 
-    pub fn get_object(&self, code: ObjectCode) -> Option<&Object> {
+    pub fn find_object(&self, code: ObjectCode) -> Option<&Object> {
         for n in 0..self.objects.len() {
             if self.objects[n].code() == code {
                 return Some(&self.objects[n]);
