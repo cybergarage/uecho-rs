4312b44866a09324b9e0247d79bf460fa7e5965c
Satoshi Konno <skonno@cybergarage.org>
2022-11-28 01:50:18 +0900

    Update comments

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,28 @@
 //! ## What is uEcho ?
 //!
 //! To implement IoT controllers or devices of [ECHONET-Lite][enet], the developer had to understand and implement the communication middleware specification such as the message format and base sequences.
-//! 
+//!
 //! The `uecho-rs` supports to control devices of [ECHONET-Lite][enet] or create the standard devices of the specification easily. Using the `uecho-rs`, the developer has only to set basic listeners to implement the devices and controllers because the `uecho-rs` handles other requests such as read and notification requests automatically.
+//!
+//! ## Getting Started
+//!
+//! To add the `uecho-rs` to your project, add the following to your Cargo.toml file:
+//!
+//! ```
+//! [dependencies]
+//! echonet = "1.x.x"
+//! ```
+//!
+//! The examples are available for [ECHONET-Lite][enet] controller and device implementations using the `uecho-rs`, check out [the examples folder in GitHub](https://github.com/cybergarage/uecho-rs/tree/master/bin).
+//!
+//! ## Getting Help
+//!
+//! - [Generated Docs (latest version)](https://docs.rs/echonet/latest/echonet/)
+//! - [Usage examples](https://github.com/cybergarage/uecho-rs/tree/master/bin).
+//!
+//! ## License
+//!
+//! This project is licensed under the Apache-2.0 License.
 //!
 //! [enet]:http://echonet.jp/english/
 
