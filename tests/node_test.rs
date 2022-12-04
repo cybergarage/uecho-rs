64ef766f81cec5393707fa2d8c1c9290817883fc
Satoshi Konno <skonno@cybergarage.org>
2022-12-04 22:55:58 +0900

    Add allow(unused_mut)

--- tests/node_test.rs
+++ tests/node_test.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(unused_mut)]
+
 use std::time::Duration;
 use std::{thread, time};
 
