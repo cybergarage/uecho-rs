305c06c0d4543b55b63c8e45517a3ec73191fcab
cybergarage <cybergarage@users.noreply.github.com>
2022-12-09 17:30:29 +0000

    deploy: e87362c1327fc506fbc2bd6ecc8ff2895f18263e

---
+++ implementors/echonet/request_handler/trait.RequestHandler.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"echonet":[],
+"monolight":[["impl <a class=\"trait\" href=\"echonet/request_handler/trait.RequestHandler.html\" title=\"trait echonet::request_handler::RequestHandler\">RequestHandler</a> for <a class=\"struct\" href=\"monolight/struct.MonoLight.html\" title=\"struct monolight::MonoLight\">MonoLight</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
