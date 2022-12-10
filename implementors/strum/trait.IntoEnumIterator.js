a318da4c185a86290486695117a286df871090e3
cybergarage <cybergarage@users.noreply.github.com>
2022-12-10 12:25:53 +0000

    deploy: a1a534a7a49ea67fd15830da54a785b46129dc5a

--- implementors/strum/trait.IntoEnumIterator.js
+++ implementors/strum/trait.IntoEnumIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"echonet":[["impl IntoEnumIterator for <a class=\"enum\" href=\"echonet/protocol/enum.Esv.html\" title=\"enum echonet::protocol::Esv\">Esv</a>"]]
+"echonet":[["impl IntoEnumIterator for <a class=\"enum\" href=\"echonet/protocol/enum.ESV.html\" title=\"enum echonet::protocol::ESV\">ESV</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
