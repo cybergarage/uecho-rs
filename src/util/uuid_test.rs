7e3d8a352f8b7a0bd39f537e8fc556fa532fbc58
Satoshi Konno <skonno@cybergarage.org>
2022-12-10 12:20:31 +0900

    Add UUID::new()

---
+++ src/util/uuid_test.rs
@@ -0,0 +1,26 @@
+// Copyright (C) 2022 The uecho-rs Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[cfg(test)]
+mod tests {
+
+    use crate::super_object::*;
+    use crate::util::uuid::*;
+
+    #[test]
+    fn uuid() {
+        let uuid = UUID::new(OBJECT_MANUFACTURER_EXPERIMENT);
+        assert_eq!(uuid.bytes().len(), 17)
+    }
+}
