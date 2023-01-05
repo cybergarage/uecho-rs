b9e32cbe60d3aa6734c6aa123ccbb1a676924e01
cybergarage <cybergarage@users.noreply.github.com>
2023-01-05 15:14:13 +0000

    deploy: e64ea165868be498b26f9ed27daefc7acaf599fa

--- implementors/echonet/transport/observer/trait.Observer.js
+++ implementors/echonet/transport/observer/trait.Observer.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"echonet":[],
-"uechodump":[["impl <a class=\"trait\" href=\"echonet/transport/observer/trait.Observer.html\" title=\"trait echonet::transport::observer::Observer\">Observer</a> for <a class=\"struct\" href=\"uechodump/struct.MyController.html\" title=\"struct uechodump::MyController\">MyController</a>"]]
+"echonet":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
