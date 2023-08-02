895f0090ce2e70ff914e4a11530268b85fe316d5
cybergarage <cybergarage@users.noreply.github.com>
2023-08-02 11:22:11 +0000

    deploy: 13315dd2755f20e7d2cd2cee47f4ab6cb267cd6d

--- implementors/log/trait.Log.js
+++ implementors/log/trait.Log.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"echonet":[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.17/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"echonet/log/struct.Logger.html\" title=\"struct echonet::log::Logger\">Logger</a>"]]
+"echonet":[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.19/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"echonet/log/struct.Logger.html\" title=\"struct echonet::log::Logger\">Logger</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
